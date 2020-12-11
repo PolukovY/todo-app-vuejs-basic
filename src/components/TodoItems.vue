ec1921dc3c9f7bbfc5186ee1e6ecbd89d034a05f
Yevgen Polukov <zhenya12@gmail.com>
2020-12-11 11:34:57 +0200

    add basic todo app with vue js

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TodoItems"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>Home page</h2>
+
+    <p>Some text</p>
+
+    <a href="/todos">Todos</a>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
